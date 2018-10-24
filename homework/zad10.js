375de8a5c5dfa377548ff00c84f2d7d3dc5eda91
mirkaNS <mirjana.g90@gmail.com>
2018-10-24 21:40:30 +0200

    new

--- homework/zad10.js
+++ homework/zad10.js
@@ -8,16 +8,20 @@ var book = function(writer,title,gener) {
 var someBook= new book("George Martin","Game of Thrones","Science fiction");
 console.log(someBook);
 
-var animal = {
-    race: "Dog",
-    age: 7,
-    name: "Dogi",
-
-};
-
-var person = {
-    name: "Pera",
-    age: 28,
-    email: "pera@gmai.com",
+var animal =function(race,age,name) {
+    this.race=race;
+    this.age=age;
+    this.name=name;
 
 }
+var someAnimal= new animal("Dog",7,"Dogi");
+console.log(someAnimal);
+
+var person = function(name,age,email) {
+    this.name=name; 
+    this.age=age; 
+    this.email=email; 
+
+}
+var somePerson= new person("Pera",28,"pera@gmai.com");
+console.log(somePerson);
