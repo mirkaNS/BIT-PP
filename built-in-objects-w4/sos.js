6921c4935d201d79cd4abd4f7eedc8b7a6f56720
BIT student <classroom@bgit.rs>
2018-10-27 15:58:09 +0200

    vezba

--- built-in-objects-w4/sos.js
+++ built-in-objects-w4/sos.js
@@ -4,8 +4,8 @@
 // Input: “1bser9re”
 // Output: false
 
-function checkIfContain5(inputString) {
-    if (inputString.indexOf('5') >= 0) {
+function checkIfContainDigit(inputString) {
+    if (inputString.indexOf("5") >= 0) {
 
         return true
     }
@@ -13,95 +13,69 @@ function checkIfContain5(inputString) {
         return false
     }
 }
-console.log(checkIfContain5('1b89abd'));
+console.log(checkIfContainDigit("1b89abd"));
 
 //2 Write a function that in a given string replaces all the appearances of the string ‘JS’ with ‘**’. 
 // Input: “Programming in JS is super interesting!”
 // Output: “Programming in ** is super interesting!”
 
-function replaceJS(inputString) {
-    var num;
-    num = inputString.indexOf('JS');
-    return inputString.slice(0, num) + '**' + inputString.slice(num + 2, inputString.length);
+function replaceString(string) {
+
+    var find = "JS";
+    return string.replace(find, "**");
 
 }
-console.log(replaceJS('Programming in JS is super interesting!'));
+console.log(replaceString("Programming in JS is super interesting!"));
 
-// TODO proveriti zasto ne radi?
-// function multiplyReplaceJS(inputString) {
-//     var num;
-//     var newString;
-//     num = inputString.indexOf('JS');
+//3.Write a function that inserts a given character on a given position in the string.
+//Input: “Goo morning”, 4, ‘d’ 
+//Output: “Good morning” 
 
-//     if (num >= 0) {
-//         (function replaceJS() {
-//             newString = '';
-//             newString = inputString.slice(0, num) + '**' + inputString.slice(num + 2, inputString.length);
+function insertsCharacter(string, index, element) {
+    var inputString = string.slice(0, index - 1) + element + string.slice(index - 1, string.length);
+    return inputString;
+}
+console.log(insertsCharacter("Goo morning", 4, "d"));
 
-//         })();
-//         return newString;
+//4.Write a function that deletes a character from the given position in the string. 
+//Input: “Goodd morning!”, 3 
+//Output: “Good morning!” 
 
-//     }
+function deletesCharacter(string, index) {
+    var find = string.charAt(index);
+    return string.replace(find, "");
+}
 
-// }
+console.log(deletesCharacter("Goodd morning!", 3));
 
+//5.Write a function that deletes every second element of the given array.
+//Input: [3, 5, 1, 8, 90, -4, 23, 1, 67]
+//Output: [3, 1, 90, 23, 67]
 
-// console.log(multiplyReplaceJS('Programming in JS is super interesting!'));
+function deletesSecondElement(array) {
 
+    for (var i = 0; i < array.length; i++) {
+        array.splice(i + 1, 1);
 
+    }
+    return array;
+}
+console.log(deletesSecondElement([3, 5, 1, 8, 90, -4, 23, 1, 67]));
 
+//6.Write a function that replaces the elements of the given array between two positions with their doubled values. 
+//Input: [3, 5, 1, 8, 90, -4, 23, 1, 67], 2, 6 
+//Output: [3, 5, 2, 16, 180, -8, 46, 1, 67]
 
-
-//3 Write a function that inserts a given character on a given position in the string.
-// Input: “Goo morning”, 4, ‘d’ 
-// Output: “Good morning” 
-
-
-// Write a function that deletes a character from the given position in the string. 
-// Input: “Goodd morning!”, 3 
-// Output: “Good morning!” 
-
-// Write a function that deletes every second element of the given array.
-// Input: [3, 5, 1, 8, 90, -4, 23, 1, 67]
-// Output: [3, 1, 90, 23, 67]
-
-// Write a function that replaces the elements of the given array between two positions with their doubled values. 
-// Input: [3, 5, 1, 8, 90, -4, 23, 1, 67], 2, 6 
-// Output: [3, 5, 2, 16, 180, -8, 46, 1, 67]
-
-
-// Write a function that checks if a given object has a given property with the given value. 
-// Input: {x: 20, y: 30}, “x”, 20 
-// Output: true
-
-
-// Input: {x: 20, y: 30}, “z”, 20 
-// Output: false 
-
-// Input: {x: 20, y: 30}, “x”, 10 
-// Output: false
+function replacesWhitDoubledValues(array, index, index2) {
+    result = [];
+    for (var i = 0; i < array.length; i++) {
+        result = array.slice(index, index2);
+        var newResult = r
+    }
+    return newResult;
+}
+console.log(replacesWhitDoubledValues([3, 5, 1, 8, 90, -4, 23, 1, 67], 2, 6));
 
 
-// Write a function that checks if every element of the first array is contained in the second array. Be careful with repetitions! 
-// Input: [3, 4, 1, 3], [8, 9, 3, 1, 11, 4, 3]
-// Output: true
 
 
-// Write a function that sorts an array of strings by the number of appearances of the letter ‘a’ or ‘A’. 
-// Input: [‘apple’, ‘tea’,  ‘amazing’, ‘morning’, ‘JavaScript’]
-// Output: [‘morning’, ‘apple’, ‘tea’, ‘JavaScript’, ‘amazing’]
-
-// Write a function that prints out the date of the next day. 
-// Output:  25. 10. 2018. 
-
-// Write a function that prints out the date of the previous day. 
-// Output:  23. 10. 2018. 
-
-
-// Write a function that prints out an array of the numbers aligned from the right side.
-// Input: [78, 111, 4, 4321]
-// Output: 
-//   78
-//  	 111  
-//     	    4
-//                       4321
